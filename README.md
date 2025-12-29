58dd807c28488cd087bafdefd22f17339258fe15
Macroover Ludvig <nest479@gmail.com>
2025-12-29 20:38:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+###### Vehicle Routing Problems with Time Windows: Bi-Level Optimization
+
+------
+This is a WIP project.
---
+++ src/main.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def main():
+    print("VRPTW environment ready")
+    print("NumPy version:", np.__version__)
+
+if __name__ == "__main__":
+    main()
